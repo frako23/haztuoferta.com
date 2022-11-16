041a97b94f76316172914cffb811b300878bfc61
PieroMastro <pier.andres@gmail.com>
2022-11-16 15:19:19 +0000

    ItemCard component version preliminar creado

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,25 +2,37 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { ItemCard } from "../component/ItemCard";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<React.Fragment>
+			<div className="text-center mt-5">
+				<h1>Hello Rigo!!</h1>
+				<p>
+					<img src={rigoImageUrl} />
+				</p>
+				<div className="container">
+					<h2 className="text-start my-4 display-4">Los Mas Buscados</h2>
+					<div className="itemcard gap-5">
+						<ItemCard />
+						<ItemCard />
+						<ItemCard />
+						<ItemCard />
+					</div>
+				</div>
+				<div className="alert alert-info">
+					{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+				</div>
+				<p>
+					This boilerplate comes with lots of documentation:{" "}
+					<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
+						Read documentation
+					</a>
+				</p>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
+		</React.Fragment>
 	);
 };
