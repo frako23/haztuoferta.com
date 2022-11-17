155f740602b38915dfdb9318fc1d1cba5379c6b9
PieroMastro <pier.andres@gmail.com>
2022-11-17 23:53:55 +0000

    Secciones creadas

---
+++ src/front/js/component/Secciones.js
@@ -0,0 +1,65 @@
+import React, { useContext } from "react";
+import PropTypes from "prop-types"
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+
+export const Secciones = () => {
+
+    const { store, actions } = useContext(Context)
+
+    return (
+        <React.Fragment>
+            <div className="container-fluid d-inline-flex">
+                <div className="col-sm-4 px-5 mt-5">
+                    <div className="seccion">
+                        <div className="image">
+                            <img src="https://dummyimage.com/600x400/f2f2f2/000000&text=QUIERO+COMPRAR" />
+                        </div>
+                        <div className="seccion-inner">
+                            <div className="header text-center">
+                                <h2>COMPRAR</h2>
+                                <h3>Haz una Oferta!</h3>
+                            </div>
+                            <div className="content">
+                                <p>Content area</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-4 px-5 mt-5">
+                    <div className="seccion">
+                        <div className="image">
+                            <img src="https://dummyimage.com/600x400/f2f2f2/000000&text=QUIERO+VENDER" />
+                        </div>
+                        <div className="seccion-inner">
+                            <div className="header text-center">
+                                <h2>VENDER</h2>
+                                <h3>Vende tu Producto!</h3>
+                            </div>
+                            <div className="content">
+                                <p>Content area</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-4 px-5 mt-5">
+                    <div className="seccion">
+                        <div className="image">
+                            <img src="https://dummyimage.com/600x400/f2f2f2/000000&text=QUIERO+CAMBIAR" />
+                        </div>
+                        <div className="seccion-inner">
+                            <div className="header text-center">
+                                <h2>INTERCAMBIAR</h2>
+                                <h3>Ofrece tu producto!</h3>
+                            </div>
+                            <div className="content">
+                                <p>Content area</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,6 +3,7 @@ import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import { ItemCard } from "../component/ItemCard";
 import { Carousel } from "../component/carousel.js";
+import { Secciones } from "../component/Secciones";
 
 
 
@@ -11,8 +12,9 @@ export const Home = () => {
 
 	return (
 		<React.Fragment>
+			<Secciones />
 			<div className="text-center mt-5">
-				<h1>Hello Rigo!!</h1>
+				<h1>Haz tu Oferta!</h1>
 				<div className="container">
 					<h2 className="text-start my-4 display-4">Los Mas Buscados</h2>
 					<div className="itemcard gap-5">
