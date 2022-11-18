0021cf383bebfd652814045734b2485ea67c745c
PieroMastro <pier.andres@gmail.com>
2022-11-18 15:39:50 +0000

    Integracion de Carrousel, Secciones y Articulos en landing page

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -12,12 +12,13 @@ export const Home = () => {
 
 	return (
 		<React.Fragment>
+			<Carousel />
 			<Secciones />
 			<div className="text-center mt-5">
-				<h1>Haz tu Oferta!</h1>
+				<h1 className="display-3">Haz tu Oferta!</h1>
 				<div className="container">
 
-					<h2 className="text-start my-4 display-4">Los Mas Buscados</h2>
+					<h2 className="text-start my-4 display-6">Los Mas Buscados</h2>
 					<div className="itemcard gap-5">
 						<ItemCard />
 						<ItemCard />
