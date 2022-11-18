aaef49f94d7a13f9960a2d6c90a8615d8e294e95
frako23 <frako23@gmail.com>
2022-11-18 22:21:19 +0000

    ajuste en el home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -5,39 +5,25 @@ import { ItemCard } from "../component/ItemCard";
 import { Carousel } from "../component/carousel.js";
 import { Secciones } from "../component/Secciones";
 
-
-
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<React.Fragment>
-			<Carousel />
-			<Secciones />
-			<div className="text-center mt-5">
-				<h1 className="display-3">Haz tu Oferta!</h1>
-				<div className="container">
-
-					<h2 className="text-start my-4 display-6">Los Mas Buscados</h2>
-					<div className="itemcard gap-5">
-						<ItemCard />
-						<ItemCard />
-						<ItemCard />
-						<ItemCard />
-					</div>
-				</div>
-				<div className="alert alert-info">
-					{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-				</div>
-				<p>
-					This boilerplate comes with lots of documentation:{" "}
-					<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-						Read documentation
-					</a>
-				</p>
-			</div>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <Carousel />
+      <Secciones />
+      <div className="text-center mt-5">
+        <h1 className="display-3">Haz tu Oferta!</h1>
+        <div className="container">
+          <h2 className="text-start my-4 display-6">Los Mas Buscados</h2>
+          <div className="itemcard gap-5">
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
-
-
