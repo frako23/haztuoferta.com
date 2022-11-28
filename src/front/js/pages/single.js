5cae6c75fb1a3b6406f209d811f6b4e8301b6891
Javierklie <javierklie1@gmail.com>
2022-11-28 11:54:05 +0000

    Cambios en Single estapa inicial

--- src/front/js/pages/single.js
+++ src/front/js/pages/single.js
@@ -5,6 +5,7 @@ import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import { Comments } from "../component/Comments";
 import "../../styles/comments.css";
+import "../../styles/single.css";
 
 export const Single = (props) => {
   const { store, actions } = useContext(Context);
@@ -12,64 +13,92 @@ export const Single = (props) => {
 
   return (
     <div className="container">
-      <div className="card mb-3 single position-relative">
-        <div className="content">
-          <div className="container">
-            <div className="row">
-              <div className="col-4 col-sm-3">
-                <div className="multi-carousel vertical">
-                  <div className="multi-carousel-inner">
-                    <div className="multi-carousel-item active my-2">
-                      <img
-                        src="https://www.shutterstock.com/image-photo/laptop-computer-mock-green-screen-600w-1959079696.jpg"
-                        alt="..."
-                        className="active w-100"
-                      />
-                    </div>
-                    <div className="multi-carousel-item mb-2">
-                      <img
-                        src="https://www.shutterstock.com/image-photo/mockup-green-screen-stanting-on-600w-1804298176.jpg"
-                        alt="..."
-                        className="w-100"
-                      />
-                    </div>
-                    <div className="multi-carousel-item mb-2">
-                      <img
-                        src="https://www.shutterstock.com/image-photo/laptop-on-desk-office-shows-600w-1804298161.jpg"
-                        alt="..."
-                        className="w-100"
-                      />
-                    </div>
-                    <div className="multi-carousel-item mb-2">
-                      <img
-                        src="https://www.shutterstock.com/image-photo/computer-peripherals-laptop-accessories-on-600w-1065152207.jpg"
-                        alt="..."
-                        className="w-100"
-                      />
-                    </div>
-                  </div>
-                  {/* <button className="carousel-control-prev" tabindex="0" type="button" data-mdb-slide="prev">
-										<span className="carousel-control-prev-icon" aria-hidden="true"></span>
-									</button>
-									<button className="carousel-control-next" tabindex="0" type="button" data-mdb-slide="next">
-										<span className="carousel-control-next-icon" aria-hidden="true"></span>
-									</button> */}
+      <div class="container mt-5 mb-5">
+        <div class="card">
+          <div class="row g-0">
+            <div class="col-md-6 border-end">
+              <div class="d-flex flex-column justify-content-center">
+                <div class="main_image">
+                  <img src="https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c07973337.png" id="main_product_image" width="350">
+                  </img>
                 </div>
-              </div>
-              <div className="col-8 col-sm-9">
-                <div className="main-img">
-                  <img
-                    src="https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c07973337.png"
-                    alt="..."
-                    className="ecommerce-gallery-main-img active w-100"
-                  />
+                <div class="thumbnail_images">
+                  <ul id="thumbnail">	
+                  <li><img onclick="changeImage(this)" src="https://www.shutterstock.com/image-photo/laptop-computer-mock-green-screen-600w-1959079696.jpg" width="70"></img></li>	
+                  <li><img onclick="changeImage(this)" src="https://www.shutterstock.com/image-photo/mockup-green-screen-stanting-on-600w-1804298176.jpg" width="70"></img></li>	
+                  <li><img onclick="changeImage(this)" src="https://www.shutterstock.com/image-photo/laptop-on-desk-office-shows-600w-1804298161.jpg" width="70"></img></li>	
+                  <li><img onclick="changeImage(this)" src="https://www.shutterstock.com/image-photo/computer-peripherals-laptop-accessories-on-600w-1065152207.jpg" width="70"></img></li>	
+                  </ul>	
                 </div>
               </div>
             </div>
-          </div>
+            <div class="col-md-6">
+              <div class="p-3 right-side">
+              <div class="d-flex justify-content-between align-items-center">	
+                <span class="heart"><i class='bx bx-heart'></i></span>	
+              </div>
+                <div class="mt-2 pr-3 content">
+                <div className="card-body">
+                  <h1 className="card-title">HP 250 G9</h1>
+                  <p className="card-text">
+                    <table className="table table-striped">
+                      <tr>
+                        <td>Pantalla</td>
+                        <td>Procesador</td>
+                        <td>Memoria Ram</td>
+                        <td>Disco Duro</td>
+                      </tr>
+                      <tr>
+                        <td>15.6"</td>
+                        <td>Intel Core I5 3.36Ghz</td>
+                        <td>DDR 3 8GB</td>
+                        <td>SSD 264 GB</td>
+                      </tr>
+                    </table>
+                  </p>
+            <hr></hr>
+            <br></br>
+                  <h5>Tipo de Oferta🤑: Subasta🔨</h5>
+                  <p>
+                    Haz tu oferta 💲:
+                    <input
+                      type="number"
+                      placeholder="Coloca tu oferta aqui"
+                      required
+                      min={1}
+                      // value={oferta}
+                      onChange={(e) => setEmail(e.target.value)}
+                      className="ms-4"
+                    />
+                  </p>
+                  <hr></hr>
+                  <br></br>
+                  <h5>Tipo de Oferta🤑: Venta🔨</h5>
+                  <p>
+                    Precio 💵: $500
+                    <button className="btn btn-primary ms-4">Comprar</button>
+                  </p>
+                </div>
+                <div class="buttons d-flex flex-row mt-5 gap-3">	
+								<button class="btn btn-outline-dark">Comprar</button>	
+								<button class="btn btn-dark">Haz tu oferta</button>
+							</div>	
+
+                </div>
+
+              </div>
+            </div>
+
         </div>
+
+
+        </div>
+        
+      </div>
+
+
         <hr></hr>
-        <div className="card-body">
+        {/* <div className="card-body">
           <h1 className="card-title">HP 250 G9</h1>
           <p className="card-text">
             <table className="table table-striped">
@@ -109,8 +138,8 @@ export const Single = (props) => {
             Precio 💵: $500
             <button className="btn btn-primary ms-4">Comprar</button>
           </p>
-        </div>
-      </div>
+        </div> */}
+
       <div>
         <Comments
           commentsUrl="https://3000-frako23-haztuofertacom-c8ycfnd9gg3.ws-us77.gitpod.io/single/1"
@@ -134,3 +163,5 @@ export const Single = (props) => {
 Single.propTypes = {
   match: PropTypes.object,
 };
+
+
