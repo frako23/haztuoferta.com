a2ec782e9dc5eaaa4d302acbd452c178f2235180
PieroMastro <pier.andres@gmail.com>
2022-11-29 16:31:10 +0000

    added banner, fixed issues with search and styles

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import "../../styles/banner.css";
 import { ItemCard } from "../component/ItemCard";
 import { Carousel } from "../component/carousel.js";
 import { Segmento } from "../component/Segmento";
@@ -13,23 +14,25 @@ export const Home = () => {
 		<React.Fragment>
 			<Carousel />
 			<Segmento />
-			<div className="text-center">
-				<figure className="image">
-					<img
-						src="https://i.imgur.com/biKBhrV.png"
-						style={{ width: "400px" }}
-						alt="Placeholder image"
-					></img>
-				</figure>
-				<div className="container">
-					<h2 className="text-center my-4 display-6">Los Mas Buscados</h2>
-					<div className="itemcard gap-5">
-						{store.computadoras.map((computadoras, index) => {
-							return <ItemCard computadoras={computadoras} key={index} />;
-						})}
+			<section className="banner">
+				<div className="container d-flex flex-column mb-2">
+					<header>
+						<h1 className="display-4 text-bold text-center">Has Tu Oferta</h1>
+						<p className="text-center text-bold"> Oferta, Intercambia o Subasta tus Productos. Que esperas para comprar a tu manera?</p>
+					</header>
+					<div className="call-to-action text-center">
+						<button className="btn btn-outline-secondary">Compra Diferente, Resgístrate Ya!</button>
 					</div>
 				</div>
+			</section>
+			<div className="container">
+				<h2 className="text-center my-4 display-6">Los Mas Buscados</h2>
+				<div className="itemcard gap-5">
+					{store.computadoras.map((computadoras, index) => {
+						return <ItemCard computadoras={computadoras} key={index} />;
+					})}
+				</div>
 			</div>
-		</React.Fragment>
+		</React.Fragment >
 	);
 };
