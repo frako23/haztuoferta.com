832a65635a4cf390e841cb5eca4fcad8172bbeda
frako23 <frako23@gmail.com>
2022-11-29 11:12:36 +0000

    se agregan funcionalidades a la seccion publsh

--- src/front/js/pages/publish.js
+++ src/front/js/pages/publish.js
@@ -16,7 +16,15 @@ export const Publish = () => {
   const [moneda, setMoneda] = useState("");
   const [precio, setPrecio] = useState("");
   const [descripcion, setDescripcion] = useState("");
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [marca, setMarca] = useState("");
+  const [modelo, setModelo] = useState("");
+  const [procesador, setProcesador] = useState("");
+  const [pantalla, setPantalla] = useState("");
+  const [memoria_ram, setMemoria_ram] = useState("");
+  const [disco_duro, setDisco_duro] = useState("");
+  const [sistema_operativo, setSistema_operativo] = useState("");
+  const [tipo_de_negocio, setTipo_de_negocio] = useState("");
+  const [nuevo_usado, setNuevo_usado] = useState("");
 
   return (
     <>
@@ -40,6 +48,82 @@ export const Publish = () => {
               }}
             />
           </InputGroup>
+          <Form.Label>Tipo de negocio</Form.Label>
+          <Form.Select
+            aria-label="dropdown select"
+            onChange={(e) => {
+              setTipo_de_negocio(e.target.value);
+            }}
+          >
+            <option disabled selected>
+              Escoge una categoria
+            </option>
+            <option value="Venta">Venta</option>
+            <option value="Intercambio">Intercambio</option>
+            <option value="Subasta">Subasta</option>
+          </Form.Select>
+          {tipo_de_negocio == "Venta" ? (
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+              <Form.Label>Precio</Form.Label>
+              <div className="d-flex">
+                <Form.Select
+                  aria-label="dropdown select"
+                  className="w-auto"
+                  onChange={(e) => {
+                    setMoneda(e.target.value);
+                  }}
+                >
+                  <option disabled selected>
+                    #
+                  </option>
+                  <option value="$">$</option>
+                  <option value="€">€</option>
+                  <option value="Bs">Bs</option>
+                </Form.Select>
+                <Form.Control
+                  type="number"
+                  placeholder="Precio"
+                  value={precio}
+                  onChange={(e) => {
+                    setPrecio(e.target.value);
+                  }}
+                />
+              </div>
+            </Form.Group>
+          ) : (
+            //  || tipo_de_negocio == "Subasta" ?
+
+            // (<Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+            //   <Form.Label>Precio</Form.Label>
+            //   <div className="d-flex">
+            //     <Form.Select
+            //       aria-label="dropdown select"
+            //       className="w-auto"
+            //       onChange={(e) => {
+            //         setMoneda(e.target.value);
+            //       }}
+            //     >
+            //       <option disabled selected>
+            //         #
+            //       </option>
+            //       <option value="$">$</option>
+            //       <option value="€">€</option>
+            //       <option value="Bs">Bs</option>
+            //     </Form.Select>
+            //     <Form.Control
+            //       type="number"
+            //       placeholder="Precio"
+            //       value={precio}
+            //       onChange={(e) => {
+            //         setPrecio(e.target.value);
+            //       }}
+            //     />
+            //   </div>
+            // </Form.Group>)
+
+            ""
+          )}
+
           <Form.Group className="mb-3 d-grid gap-2" controlId="formFile">
             <Form.Label>Agregar fotos </Form.Label>
             <CloudinaryUploadWidget />
@@ -78,35 +162,103 @@ export const Publish = () => {
               <option value="ropa">Ropa y Zapatos</option>
               <option value="otros">Otras categorías</option>
             </Form.Select>
-            {/* {categoria == "computadoras" ?} */}
+            <br></br>
+            {categoria == "computadoras" ? (
+              <>
+                {" "}
+                <InputGroup className="mb-3">
+                  <InputGroup.Text id="basic-addon1">Marca</InputGroup.Text>
+                  <Form.Control
+                    placeholder="Marca"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                    value={marca}
+                    onChange={(e) => {
+                      setMarca(e.target.value);
+                    }}
+                  />
+
+                  <InputGroup.Text id="basic-addon1">Modelo</InputGroup.Text>
+                  <Form.Control
+                    placeholder="Modelo"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                    value={modelo}
+                    onChange={(e) => {
+                      setModelo(e.target.value);
+                    }}
+                  />
+                </InputGroup>
+                <InputGroup className="mb-3">
+                  <InputGroup.Text id="basic-addon1">
+                    Procesador
+                  </InputGroup.Text>
+                  <Form.Control
+                    placeholder="Procesador"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                    value={procesador}
+                    onChange={(e) => {
+                      setProcesador(e.target.value);
+                    }}
+                  />
+                  <InputGroup.Text id="basic-addon1">Pantalla</InputGroup.Text>
+                  <Form.Control
+                    placeholder="Pantalla"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                    value={pantalla}
+                    onChange={(e) => {
+                      setPantalla(e.target.value);
+                    }}
+                  />
+                </InputGroup>
+                <InputGroup className="mb-3">
+                  <InputGroup.Text id="basic-addon1">
+                    Disco duro
+                  </InputGroup.Text>
+                  <Form.Control
+                    placeholder="Disco duro"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                    value={disco_duro}
+                    onChange={(e) => {
+                      setDisco_duro(e.target.value);
+                    }}
+                  />
+                  <InputGroup.Text id="basic-addon1">
+                    Memoria Ram
+                  </InputGroup.Text>
+                  <Form.Control
+                    placeholder="Memoria Ram"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                    value={memoria_ram}
+                    onChange={(e) => {
+                      setMemoria_ram(e.target.value);
+                    }}
+                  />
+                </InputGroup>
+                <Form.Label>Sistema Operativo</Form.Label>
+                <Form.Select
+                  aria-label="dropdown select"
+                  onChange={(e) => {
+                    setSistema_operativo(e.target.value);
+                  }}
+                >
+                  <option disabled selected>
+                    Escoge una categoria
+                  </option>
+                  <option value="Windows">Windows</option>
+                  <option value="macOS">macOS</option>
+                  <option value="ChromeOS">ChromeOS</option>
+                </Form.Select>{" "}
+              </>
+            ) : (
+              ""
+            )}
           </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
-            <Form.Label>Precio</Form.Label>
-            <div className="d-flex">
-              <Form.Select
-                aria-label="dropdown select"
-                className="w-auto"
-                onChange={(e) => {
-                  setMoneda(e.target.value);
-                }}
-              >
-                <option disabled selected>
-                  #
-                </option>
-                <option value="$">$</option>
-                <option value="€">€</option>
-                <option value="Bs">Bs</option>
-              </Form.Select>
-              <Form.Control
-                type="number"
-                placeholder="Precio"
-                value={precio}
-                onChange={(e) => {
-                  setPrecio(e.target.value);
-                }}
-              />
-            </div>
-          </Form.Group>
+
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>Descripción</Form.Label>
             <Form.Control
