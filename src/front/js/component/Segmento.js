d95b3c2d535a769afdb445d6a558fc38e3d0f239
frako23 <frako23@gmail.com>
2022-11-30 19:33:07 +0000

    cambio en segmento

--- src/front/js/component/Segmento.js
+++ src/front/js/component/Segmento.js
@@ -1,84 +1,88 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/segmento.css";
+import comprar from "../../img/compras.jpg";
+import vender from "../../img/vende.jpg";
+import intercambiar from "../../img/Screenshot 2022-11-30 3.17.53 PM.png";
+import subastar from "../../img/Screenshot 2022-11-30 3.21.02 PM.png";
 
 export const Segmento = () => {
-	return (
-		<React.Fragment>
-			<section className="section-products mb-2">
-				<div className="container">
-					<div className="row">
-						<div className="col-md-6 col-lg-4 col-xl-3">
-							<Link className="seccion-link" to="/demo">
-								<div className="seccion">
-									<div className="image">
-										<span className="img-card">
-											<img src="https://placehold.co/600x400?text=QUIERO+VENDER" />
-										</span>
-									</div>
-									<div className="seccion-inner text-center pt-5">
-										<div className="header text-center">
-											<h2>VENDE</h2>
-											<h3>Vende tu Producto!</h3>
-										</div>
-									</div>
-								</div>
-							</Link>
-						</div>
-						<div className="col-md-6 col-lg-4 col-xl-3">
-							<Link className="seccion-link" to="/oferts">
-								<div className="seccion">
-									<div className="image">
-										<span className="img-card">
-											<img src="https://placehold.co/600x400?text=QUIERO+COMPRAR" />
-										</span>
-									</div>
-									<div className="seccion-inner text-center pt-5">
-										<div className="header text-center">
-											<h2>COMPRA</h2>
-											<h3>Haz una Oferta!</h3>
-										</div>
-									</div>
-								</div>
-							</Link>
-						</div>
-						<div className="col-md-6 col-lg-4 col-xl-3">
-							<Link className="seccion-link" to="/demo">
-								<div className="seccion">
-									<div className="image">
-										<span className="img-card">
-											<img src="https://placehold.co/600x400?text=QUIERO+CAMBIAR" />
-										</span>
-									</div>
-									<div className="seccion-inner text-center pt-5">
-										<div className="header text-center">
-											<h2>INTERCAMBIA</h2>
-											<h3>Porpón un Trueque!</h3>
-										</div>
-									</div>
-								</div>
-							</Link>
-						</div>
-						<div className="col-md-6 col-lg-4 col-xl-3">
-							<Link className="seccion-link" to="/demo">
-								<div className="seccion">
-									<div className="image">
-										<span className="img-card">
-											<img src="https://placehold.co/600x400?text=QUIERO+SUBASTAR" />
-										</span>
-									</div>
-									<div className="seccion-inner text-center pt-5">
-										<div className="header text-center">
-											<h2>SUBASTA</h2>
-											<h3>Subasta tu Producto!</h3>
-										</div>
-									</div>
-								</div>
-							</Link>
-						</div>
-					</div>
-				</div>
-			</section >
-		</React.Fragment >
-	);
+  return (
+    <React.Fragment>
+      <section className="section-products mb-2">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-lg-4 col-xl-3">
+              <Link className="seccion-link" to="/demo">
+                <div className="seccion">
+                  <div className="image">
+                    <span className="img-card">
+                      <img src={vender} />
+                    </span>
+                  </div>
+                  <div className="seccion-inner text-center pt-5">
+                    <div className="header text-center">
+                      <h2>VENDE</h2>
+                      <h3>Vende tu Producto!</h3>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+            <div className="col-md-6 col-lg-4 col-xl-3">
+              <Link className="seccion-link" to="/oferts">
+                <div className="seccion">
+                  <div className="image">
+                    <span className="img-card">
+                      <img src={comprar} />
+                    </span>
+                  </div>
+                  <div className="seccion-inner text-center pt-5">
+                    <div className="header text-center">
+                      <h2>COMPRA</h2>
+                      <h3>Haz una Oferta!</h3>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+            <div className="col-md-6 col-lg-4 col-xl-3">
+              <Link className="seccion-link" to="/demo">
+                <div className="seccion">
+                  <div className="image">
+                    <span className="img-card">
+                      <img src={intercambiar} />
+                    </span>
+                  </div>
+                  <div className="seccion-inner text-center pt-5">
+                    <div className="header text-center">
+                      <h2>INTERCAMBIA</h2>
+                      <h3>Porpón un Trueque!</h3>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+            <div className="col-md-6 col-lg-4 col-xl-3">
+              <Link className="seccion-link" to="/demo">
+                <div className="seccion">
+                  <div className="image">
+                    <span className="img-card">
+                      <img src={subastar} />
+                    </span>
+                  </div>
+                  <div className="seccion-inner text-center pt-5">
+                    <div className="header text-center">
+                      <h2>SUBASTA</h2>
+                      <h3>Subasta tu Producto!</h3>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
 };
