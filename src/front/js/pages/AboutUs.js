8058f0c19add96f72a52c29336b7d1ab4e209310
frako23 <frako23@gmail.com>
2022-12-06 10:31:50 +0000

    ajuste en about us

--- src/front/js/pages/AboutUs.js
+++ src/front/js/pages/AboutUs.js
@@ -30,11 +30,10 @@ export const AboutUs = () => {
               </span>{" "}
             </p>
             <p className="fs-5 text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, id
-              dolorem molestiae maiores autem, minus amet libero nihil odit
-              consectetur, provident magni harum fuga tempore sunt praesentium
-              magnam architecto. Quibusdam.
+              37 años, Ingeniero de Producción de la USB, con mas de 13 años de
+              experiencia en marketing y ventas, papá de Sebastián
             </p>
+            <p className="fs-5 text">Siempre viculado a la tecnología</p>
           </div>
           <div className="col-4 w-auto">
             <img
@@ -63,9 +62,11 @@ export const AboutUs = () => {
               Piero Mastrogiacomo<span className="text-secondary"> CEO</span>{" "}
             </p>
             <p className="fs-5 text">
-              Medico de profesión, 40 años. Orgulloso padre.</p>
+              Medico de profesión, 40 años. Orgulloso padre.
+            </p>
             <p className="fs-5 text">
-              Amante de la tecnologia, en busca de un giro profesional de 180 grados.
+              Amante de la tecnologia, en busca de un giro profesional de 180
+              grados.
             </p>
           </div>
         </div>
@@ -122,6 +123,23 @@ export const AboutUs = () => {
               dolorem molestiae maiores autem, minus amet libero nihil odit
               consectetur, provident magni harum fuga tempore sunt praesentium
               magnam architecto. Quibusdam.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="container py-5 border-top ">
+        <div className="row d-flex justify-content-center ">
+          <div className="col-4 w-auto">
+            <img
+              src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_960_720.png"
+              alt="imagen"
+              width="300px"
+              style={{ "border-radius": "50%" }}
+            />
+          </div>
+          <div className="col-8">
+            <p className="fs-1 text fw-bolder">
+              Tecnologias que usamos<span className="text-secondary"> CEO</span>{" "}
             </p>
           </div>
         </div>
