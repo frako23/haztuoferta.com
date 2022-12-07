3c25829f742c16d1014c951ef31590e0e182adbd
Javierklie <javierklie1@gmail.com>
2022-12-07 19:45:07 +0000

    area publish

--- src/front/js/component/Segmento.js
+++ src/front/js/component/Segmento.js
@@ -19,16 +19,7 @@ export const Segmento = () => {
           <br></br>
           <div className="containersegmento d-flex justify-content-center">
             <p className="lead text-bold text-center">
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using 'Content here,
-              content here', making it look like readable English. Many desktop
-              publishing packages and web page editors now use Lorem Ipsum as
-              their default model text, and a search for 'lorem ipsum' will
-              uncover many web sites still in their infancy. Various versions
-              have evolved over the years, sometimes by accident, sometimes on
-              purpose{" "}
+            Encontraras todo tipo de productos con opcion a intercambio y precios a justados al mercado
             </p>
           </div>
 
